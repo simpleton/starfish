4de6498fd77223114ed30d13263573f958134453
xbfool <xbfool@gmail.com>
2012-08-16 15:30:54 +0800

    add html code

--- www/js/starfish.js
+++ www/js/starfish.js
@@ -5,3 +5,14 @@
  * Time: AM10:27
  * To change this template use File | Settings | File Templates.
  */
+var hello_test = function() {
+    $.ajax({
+        type:"GET",
+        url:$.starfishd_url + "/hello?" + $('#param').val(),
+        contentType: "application/json; charset=utf-8",
+        success:function(data) {
+            $("#result").html(data);
+        },
+        dataType:"html"
+    });
+};
